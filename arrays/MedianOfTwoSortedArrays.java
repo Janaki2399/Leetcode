cba7f799bcc24cc5daa396f1ee827b16c64b1012
Janaki <janakishreeja23@gmail.com>
2020-05-04 19:14:25 +0530

    commit

--- arrays/MedianOfTwoSortedArrays.java
+++ arrays/MedianOfTwoSortedArrays.java
@@ -1,3 +1,23 @@
+/*There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+*/
+//Brute force Approach: Time Complexity -O(n)
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int k=0;
@@ -62,3 +82,79 @@ class Solution {
         return median;
     }
 }
+
+//Efficient Approach using Binary Search  Time COmplexity -O(log(m+n))
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+       if(nums1.length>nums2.length)
+       {
+           return findMedianSortedArrays(nums2,nums1);
+       }
+        int low=0;
+        int high=nums1.length;
+        int leftX=0,leftY=0,RightX=0,RightY=0;
+        while(low<=high)
+        {
+            int partitionX=(low+high)/2;
+            int partitionY=((nums1.length+nums2.length+1)/2)-partitionX;
+            
+            if(partitionX==0)
+            {
+                leftX=Integer.MIN_VALUE;
+            }
+            else
+            {
+                leftX=nums1[partitionX-1];
+            }
+              if(partitionX==nums1.length)
+            {
+                RightX=Integer.MAX_VALUE;
+            }
+            else
+            {
+                RightX=nums1[partitionX];
+            }
+            
+              if(partitionY==0)
+            {
+                leftY=Integer.MIN_VALUE;
+            }
+            else
+            {
+                leftY=nums2[partitionY-1];
+            }
+            if(partitionY==nums2.length)
+            {
+                RightY=Integer.MAX_VALUE;
+            }
+            else
+            {
+                RightY=nums2[partitionY];
+            }
+            
+        if(leftX<=RightY && leftY<=RightX)
+        {
+            if((nums1.length+nums2.length)%2==0)
+            {
+                return ((double) Math.max(leftX,leftY)+Math.min(RightX,RightY))/2;
+            }
+            else
+            {
+               return (double) Math.max(leftX,leftY);
+            }
+        }
+            else if(leftX>RightY)
+            {
+                high=partitionX-1;
+                
+            }
+            else
+            {
+                low=partitionX+1;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
+
+
