52b621cef2cddb37405dfa1c71c0c3b90fa78b2c
janaki <janakishreeja23@gmail.com>
2020-06-27 20:12:35 +0530

    commit

---
+++ arrays/PowerOfTwo.java
@@ -0,0 +1,34 @@
+/*Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+
+Input: 1
+Output: true 
+Explanation: 20 = 1
+Example 2:
+
+Input: 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: 218
+Output: false*/
+
+Solution:
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(n<=0)
+        {
+            return false;
+        }
+        
+        while(n%2==0)
+        {
+            n=n/2;
+        }
+      return n==1;
+    }
+}
+
